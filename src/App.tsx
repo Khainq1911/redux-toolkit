118a63428813d07543eccc5e653438524ad971f3
KhaiNguyen <khaitom1911@.dev@gmail.com>
2025-07-09 17:16:38 +0700

    todo list

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import CounterPage from "./pages/counter/counterPages";
+import TodoPage from "./pages/todo/todoPage";
 
 function App() {
   return (
     <div className="app">
       <CounterPage />
+      <TodoPage />
     </div>
   );
 }
